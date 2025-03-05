92f793b639d2507e3936a820288332df33fbc9e5
风风 <qwe18173945756@163.com>
2025-03-05 23:35:44 +0800

    feat:实现了二级路由

--- template/web-cli/src/views/home.vue
+++ template/web-cli/src/views/home.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="home-container flex  w-full h-screen !py-[8%]" >
+    <div class="home-container flex  w-full h-screen !py-[8%]">
         <div class="left flex justify-center items-center flex-col" style="flex: 1;">
             <div class="img"><img src="@/assets/images/2.gif" alt="这是gif图" class=" img w-60 h-60"></div>
             <div class="">
@@ -14,37 +14,44 @@
                 </div>
             </div>
         </div>
-        <div class="right flex-1 flex justify-center  border-l border-[#42b883] " >
-            <div class="doThing" style="font-size: 14px;display: flex;justify-content: center;width: 96%;height: 40px;align-items: center;gap: 10px;">
-                <div class="none" style="flex: 1;"></div>
-                <div class="study"style="background-color: aqua;flex: 1;text-align: center;border-radius: 5px;" @click="changeRouter('study')">学习</div>
-                <div class="none" style="flex: 1;"></div>
-                <div class="work" style="background-color: aqua;flex: 1; text-align: center;" @click="changeRouter('work')">工作</div> 
-                <div class="none" style="flex: 1;"></div>
+        <div class="right flex-1 flex items-center flex-col  border-l border-[#42b883] gap-[40px]" >
+            <div class="doThing  flex justify-center items-center w-[96%] h-10 text-sm gap-2.5 mx-auto">
+                <div class="none flex1" ></div> 
+                <div class="study flex-1 text-center cursor-pointer"
+                    @click="changeRouter('study')" :class="{'selected':checked === 'study'}">学习</div>
+                <div class="none flex1"></div>
+                <div class="work flex-1 text-center cursor-pointer" 
+                    @click="changeRouter('work')" :class="{'selected':checked === 'work' }">工作</div>
+                <div class="none flex1" ></div>
             </div>
             <div class="getthing">
-                <RouterView/>
+                <RouterView />
             </div>
         </div>
-
-
     </div>
 </template>
 
 <script setup lang='ts'>
-import { onMounted, ref,  } from 'vue';
-import { RouterView ,useRouter} from 'vue-router';
+import { onMounted, ref, } from 'vue';
+import { RouterView, useRouter } from 'vue-router';
 const $router = useRouter()
-
+const checked = ref('study')
 //h-[100px]
 
 const goNewWindow = (event: Event) => {
     event.preventDefault();
     window.open("https://gitee.com/fengqikengni", '_blank', 'noopener,noreferrer')
 }
-const changeRouter = (url:string)=>{
-    $router.push({path:`/${url}`})
+const changeRouter = (url: string) => {
+    checked.value = url
+    $router.push({ path: `/${url}` })
 }
 </script>
 
-<style scoped></style>
+<style scoped>
+.selected {
+    background-color: #5d5cde;
+    color: aliceblue;
+    border-radius: 5px;
+}
+</style>
--- template/web-cli/src/views/study.vue
+++ template/web-cli/src/views/study.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="study-container">
-        <img :src="study" alt="" class="img h-[50px] w-[50px]">
+        <img :src="study" alt="" class="img h-[360px] w-[360px] rounded-[20px]">
     </div>
 </template>
 
